58bd1f7d97ff394328d5ca6f96fe0e6dcabb2a97
Zach White <zachwhite99@gmail.com>
2020-10-11 17:02:15 -0400

    Travis-CI integration
    Added .travis.yml file, added some dummy tests for travis to run, and html changes to verify successful heroku deployment

--- civicconnect/tests.py
+++ civicconnect/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+'''
+dummy test for travis-CI setup
+From Mark Sheriff StaffTextbookExchange-f20
+'''
+class DummyTestCase(TestCase):
+    def setUp(self):
+        x = 1
+    
+    def test_dummy_test_case(self):
+        self.assertEqual(1, 1)
+        
