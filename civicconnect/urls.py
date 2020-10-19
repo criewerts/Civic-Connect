41b481c2e684ddf2151d7782061730983589d566
neilmenon <nkm5gz@virginia.edu>
2020-10-18 21:28:32 -0400

    (1) created templates and topic views, (2) improved login integration, (3) added style

--- civicconnect/urls.py
+++ civicconnect/urls.py
@@ -5,4 +5,9 @@ from . import views
 app_name = 'civicconnect'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('templates/', views.TemplateIndexView.as_view(), name='template_index'),
+    path('templates/create/', views.TemplateCreateView.as_view(), name='template_create'),
+    path('templates/<int:pk>/', views.TemplateDetailView.as_view(), name='template_detail'),
+    path('topics/', views.TopicIndexView.as_view(), name='topic_index'),
+    path('topics/<int:pk>/', views.TopicDetailView.as_view(), name='topic_detail'),
 ]
